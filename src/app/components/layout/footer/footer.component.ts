06e27ca2d6e6e3b5c62744bd08e36399e7abc4f3
3zr4th <dayelin@xetid.com>
2025-08-10 01:16:08 -0400

    fix +

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 import { Router, NavigationEnd, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
+
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterModule, CommonModule, TranslocoModule,],
+  imports: [RouterModule, CommonModule, TranslocoModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
   currentYear: number = new Date().getFullYear();
   currentRoute = '';
-
 }
