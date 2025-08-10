f8ead98ade0d6dfb8abc521559eb3dc243277fc5
3zr4th <dayelin@xetid.com>
2025-08-10 09:23:11 -0400

    modified:   src/environments/environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,4 @@ export const environment = {
   api_users: `${apiUrl}users/`,
   api_security: `${apiUrl}auth/`,
   api_img: `${imgUrl}`,
-  FRONT_TOKEN:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQHNoaXJrYXNvZnQuY29tIiwiZnVsbF9uYW1lIjoiU3VwZXIgQWRtaW4iLCJjbGllbnQiOiJzaGlya2Fzb2Z0In0.aJzrR79pG1NhTqRTW4fk0NmmmR7F_XVj0jKQ8c1URhg',
 };
