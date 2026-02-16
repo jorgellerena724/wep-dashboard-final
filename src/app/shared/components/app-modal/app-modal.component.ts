3182497e8c87dfb569d672731bbc134ab00ff852
3zr4th <juanpmm9810@gmail.com>
2026-02-16 10:35:06 -0500

    funcionalidad de expandir y contraer modal

--- src/app/shared/components/app-modal/app-modal.component.ts
+++ src/app/shared/components/app-modal/app-modal.component.ts
@@ -49,6 +49,10 @@ export class ModalComponent {
 
   // Signal para manejar tamaño maximo
   maxContentHeight = signal('300px');
+
+  // Signal para estado expandido
+  isExpanded = signal(false);
+
   // Suscripciones para manejo manual
   private formValidSub?: Subscription;
   private submitSuccessSub?: Subscription;
@@ -185,11 +189,16 @@ export class ModalComponent {
     this.visible.set(false);
     this.isProcessing.set(false);
     this.loading.set(false);
+    this.isExpanded.set(false);
 
     if (this.componentRef) {
       this.componentRef.destroy();
       this.componentRef = null;
     }
+  }
+
+  toggleExpand() {
+    this.isExpanded.update((expanded: boolean) => !expanded);
   }
 
   onAccept() {
